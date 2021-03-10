db5c06f5a521ce3a6812a9326109162d89679ef9
Andrei Drobysh <andrey.drobysh.04@gmail.com>
2021-03-10 20:15:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (arr == "" || !arr) {
+    return 0;
+  }
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
+
+exports.max = function max (arr) {
+  if (arr == "" || !arr) {
   return 0;
 }
+let max = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+}
+return max;
+}
 
-exports.avg = function avg (array) {
+exports.avg = function avg (arr) {
+  if (arr == "" || !arr) {
   return 0;
+}
+let main = 0;
+let avg = 0;
+for (let i = 0; i < arr.length; i++) {
+  avg += arr[i]
+}
+main = avg / arr.length;
+return main;
+
 }
